d6c680ff592837f4b77ee3775bda5b03355e2b89
kjstarun <kjstarun@gmail.com>
2023-07-13 20:47:53 +0530

    updated useeffect with console

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -11,7 +11,7 @@ const Feed = () => {
   const PromptCardList = () => {
     return (
       <div className="mt-16 prompt_layout">
-        {filteredPosts.length === 0
+        {filteredPosts.length === 0 || posts.length >= 1
           ? posts.map((item, index) => <PromptCard post={item} key={index} />)
           : filteredPosts.map((item, index) => (
               <PromptCard post={item} key={index} />
