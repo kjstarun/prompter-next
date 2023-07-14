5bf17ea2ae5f0a6aa34d81c26dda5991e97fcc6e
kjstarun <kjstarun@gmail.com>
2023-07-14 17:54:24 +0530

    updated with server side api calling

--- components/PromptCardList.jsx
+++ components/PromptCardList.jsx
@@ -4,8 +4,8 @@ const PromptCardList = ({data}) => {
   console.log("hello from propmtcardlist", data);
   return (
     <div className="mt-16 prompt_layout">
-      {posts &&
-        posts.map((item, index) => <PromptCard post={item} key={index} />)}
+      {data &&
+        data.map((item, index) => <PromptCard post={item} key={index} />)}
       {/* {filteredPosts.length === 0
           ? posts && posts.map((item, index) => <PromptCard post={item} key={index} />)
           : filteredPosts.map((item, index) => (
